707bd16d34f51c90e2780b22754af027866d308f
Pekofirst <1758244835@qq.com>
2026-01-08 11:07:54 +0800

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -8,18 +8,23 @@ class MCpingCommand(BaseCommand):
     command_description = "这是一个查询我的世界服务器状态的命令"
     command_pattern = r"^信息$"
     async def execute(self) -> Tuple[bool,  bool]:
-            server = JavaServer.lookup("pekoserver.tech:18221")
-            status = server.status()
-            query = server.query()
-            message = f"♥ 当前服务器人数：{status.players.online}服务器延迟{status.latency}♥\n♥在线玩家：{', '.join(query.players.names)}♥"
-            await self.send_text(message)
-            # ---- 执行命令的逻辑 ----
-            return True, True  
-
+            try:
+                server = JavaServer.lookup("pekoserver.tech:18221")
+                status = server.status()
+                message = f"♥ 当前服务器人数：{status.players.online}服务器延迟{status.latency}♥"
+                query = server.query()
+                if query.players.list:
+                    # 有完整列表则直接打印
+                    message+=f"\n♥ 在线玩家：{', '.join(query.players.names)} ♥"
+                else:
+                    pass
+                await self.send_text(message)
+                return True, True
+            except Exception as e:
+                    await self.send_text(f"♥ 查询出错：{str(e)} ♥")
+                    return False, True
 @register_plugin # 注册插件
-class HelloWorldPlugin(BasePlugin):
-    """Hello World插件 - 你的第一个MaiCore插件"""
-
+class MCpingPlugin(BasePlugin):
     # 以下是插件基本信息和方法（必须填写）
     plugin_name = "mcping"
     enable_plugin = True  # 启用插件
@@ -31,4 +36,3 @@ class HelloWorldPlugin(BasePlugin):
     def get_plugin_components(self) -> List[Tuple[ComponentInfo, Type]]: # 获取插件组件
         return [MCpingCommand.get_command_info(),MCpingCommand]
     
-
